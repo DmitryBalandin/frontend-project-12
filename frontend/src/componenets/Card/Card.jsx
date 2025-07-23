5c995bd0deddd214c024ad42fa4807b66162bfd1
DmitryBalandin <balandinqwerty1983@gmail.com>
2025-07-23 21:58:46 +0500

    step2

--- frontend/src/componenets/Card/Card.jsx
+++ frontend/src/componenets/Card/Card.jsx
@@ -5,8 +5,9 @@ function Card({ children, image, titleImage, footer }) {
     return (
 
         <div className="card">
-            <div className="card-body">
-                <img src={image} className="rounded-circle" alt={titleImage} />
+            <div className="card-body d-flex flex-column flex-md-row flex-wrap flex-md-nowrap justify-content-between align-items-center m-5
+            gap-4 ">
+                <img src={image} className="rounded-circle " alt={titleImage} />
                 {children}
             </div>
             {
--- frontend/src/componenets/FormRegistartion/FormRegistration.jsx
+++ frontend/src/componenets/FormRegistartion/FormRegistration.jsx
@@ -2,12 +2,17 @@ import { Formik, Form, Field } from 'formik';
 
 
 const FormRegisrtation = () => {
-    return (<div>
+    return (<div className='flex-grow-1 align-self-stretch '>
         <h1>Войти</h1>
         <Formik initialValues={{
-            username: 'wwq',
+            username: '',
             password: ''
-        }}>
+        }}
+        onSubmit={values => {
+         // same shape as initial values
+         console.log(values);
+       }}
+        >
             {() => (
                 <Form>
                     <Field
@@ -16,14 +21,17 @@ const FormRegisrtation = () => {
                         name='username'
                     />
                     <Field
-                     className='form-control'
+                     className='form-control mb-3'
                         type='password'
                         name='password'
                     />
+                    <button type="submit" class="btn btn-outline-primary w-100">Войти</button>
                 </Form>
+                
             )}
-
+            
         </Formik>
+        
     </div>)
 }
 
