962bf4b80a4e5c906ef510a45c599a6170a67e40
DmitryBalandin <balandinqwerty1983@gmail.com>
2025-08-23 21:04:31 +0500

    asdas

--- frontend/src/componenets/Navigation/Navigator.jsx
+++ frontend/src/componenets/Navigation/Navigator.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 
 function Navigation({children}) {
+    console.log('asas')
     return (
         <nav className="navbar shadow-sm">
                 <Link className="navbar-brand px-4" to="/">
