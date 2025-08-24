acaed49e35ee8ed077f9f7f163f64fc3bcefeb07
DmitryBalandin <balandinqwerty1983@gmail.com>
2025-08-24 20:18:04 +0500

    887

--- frontend/src/componenets/Channels/Channels.jsx
+++ frontend/src/componenets/Channels/Channels.jsx
@@ -9,16 +9,16 @@ import RemoveModal from '../Modals/RemoveModal';
 import { selectors as selectorsChannels } from '../../slices/channelsSlice';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import LeoProfanity from 'leo-profanity'
+// import LeoProfanity from 'leo-profanity'
 
 
 const Channels = ({ channels, setActiveChannel, activeChannel, setIsHost }) => {
     const { t } = useTranslation();
     const [showModal, setShowModal] = useState(true)
     const [dateModal, setDateModal] = useState({ type: null, id: null })
-    useEffect(() => {
-        LeoProfanity.loadDictionary('ru')
-    }, [])
+    // useEffect(() => {
+    //     LeoProfanity.loadDictionary('ru')
+    // }, [])
 
     const listNamesChannels = useSelector(state => selectorsChannels.selectAll(state))
         .map(({ name }) => name)
@@ -92,9 +92,11 @@ const Channels = ({ channels, setActiveChannel, activeChannel, setIsHost }) => {
                         <Dropdown as={ButtonGroup} className=' w-100'>
                             <Button onClick={() => handleClick(id)} className={`w-100 rounded-0 text-start text-black border-0 text-truncate${activeChannel === id ? ' btn-secondary text-white' : ' bg-light '}`}>
                                 <span className="me-1">#</span>
-                                {LeoProfanity.clean(name)}
+                                {name}
                             </Button>
-                            <Dropdown.Toggle className={`border-0 ${activeChannel === id ? ' bg-secondary text-white' : ' bg-light text-black'}`} split id="dropdown-split-basic" />
+                            <Dropdown.Toggle className={`border-0 ${activeChannel === id ? ' bg-secondary text-white' : ' bg-light text-black'}`} split id="dropdown-split-basic" >
+                                <span className='visually-hidden'>Управление каналом</span>
+                            </Dropdown.Toggle >
                             <Dropdown.Menu >
                                 <Dropdown.Item as={Button} onClick={() => handleChannel({ type: 'remove', id })}>{t('buttonActionName.remove')}</Dropdown.Item>
                                 <Dropdown.Item as={Button} onClick={() => handleChannel({ type: 'rename', id })}>{t('buttonActionName.rename')}</Dropdown.Item>
