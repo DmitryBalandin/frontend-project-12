d0aaadb428ee4495d7519e391c15704ecd34070e
DmitryBalandin <balandinqwerty1983@gmail.com>
2025-08-24 17:06:44 +0500

    993

--- frontend/src/componenets/Channels/Channels.jsx
+++ frontend/src/componenets/Channels/Channels.jsx
@@ -66,7 +66,8 @@ const Channels = ({ channels, setActiveChannel, activeChannel, setIsHost }) => {
             <div className="d-flex mt-1 justify-content-around mb-2 ps-4 pe-2 p-4">
                 <b className='text-muted'>{t('phrase.channels')}</b>
                 <button type="button" className="p-0 text-primary btn btn-group-vertical" onClick={() => handleChannel({ type: 'add' })}>
-                    <img src={crossInSquare} width="23" height="23" className="bg-secondary" alt="add channels" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor" class="bi bi-plus-square"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"></path><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path></svg>
+                    {/* <img src={crossInSquare} width="23" height="23" className="bg-secondary" alt="add channels" /> */}
                 </button>
             </div>
             <ul id='channels-box' className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
